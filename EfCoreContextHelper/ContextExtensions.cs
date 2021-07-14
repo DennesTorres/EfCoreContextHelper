ac23c62f0ea1a990ec3a44e0199407d76b8c3c19
Dennes Torres <dennes@bufaloinfo.com.br>
2021-07-14 10:14:11 +0000

    new file:   ContextExtensions.cs 	new file:   EfCoreContextHelper.csproj

---
+++ EfCoreContextHelper/ContextExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace libBaseData
+{
+    public static class ContextExtensions
+    {        
+
+        private static List<Action<IMutableEntityType>> Conventions=new List<Action<IMutableEntityType>>();
+
+        public static void AddRemovePluralizeConvention(this ModelBuilder builder)
+        {
+                Conventions.Add(et => et.SetTableName(et.DisplayName()));
+        }
+
+        public static void AddRemoveOneToManyCascadeConvention(this ModelBuilder builder)
+        {
+            Conventions.Add(et => et.GetForeignKeys()
+                    .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade)
+                    .ToList()
+                    .ForEach(fk => fk.DeleteBehavior = DeleteBehavior.Restrict));
+        }
+
+        public static void ApplyConventions(this ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                foreach(Action<IMutableEntityType> action in Conventions)
+                    action(entityType);
+            }
+
+            Conventions.Clear();
+        }
+    }
+}
